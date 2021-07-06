37ece5e87ad8081c8b8d2cbf1c01d9a652422b4f
Jared Leonard <jleona15@binghamton.edu>
2021-07-06 14:26:51 -0400

    Restructured the Memory class to store state-action-reward's in a way that more easily allows discounting rewards at the end of each episode. Removed swathes of debug code.

--- Tetris RL/Tetris_RL.py
+++ Tetris RL/Tetris_RL.py
@@ -21,19 +21,6 @@ BOARD_EMPTY = 0.
 BOARD_OCCUPIED = 1.
 BOARD_ACTIVE = 2.
 
-masks = [
-    Image.open("mask0.bmp").convert('L').getdata(),
-    Image.open("mask1.bmp").convert('L').getdata(),
-    Image.open("mask2.bmp").convert('L').getdata(),
-    Image.open("mask3.bmp").convert('L').getdata(),
-    Image.open("mask4.bmp").convert('L').getdata(),
-    Image.open("mask5.bmp").convert('L').getdata(),
-    Image.open("mask6.bmp").convert('L').getdata(),
-    Image.open("mask7.bmp").convert('L').getdata(),
-    Image.open("mask8.bmp").convert('L').getdata(),
-    Image.open("mask9.bmp").convert('L').getdata()
-]
-
 action_map = [
     "rleft",
     "rright",
@@ -48,7 +35,7 @@ class TetrisSimulation:
 
     def clear(self):
         self.score = 0
-        self.board = np.zeros((10, 20))
+        self.board = np.zeros((10, 20), dtype=np.uint8)
         self.active_indices = []
         self.active_type = 0
         self.rotation_state = 0
@@ -438,8 +425,6 @@ class TetrisSimulation:
             else:
                 self.rotation_state = 0
 
-
-
         return True
 
     def printBoard(self):
@@ -543,26 +528,17 @@ class TetrisSimulation:
             self.score += 300
         elif clear_count == 4:
             self.score += 1200
-        #else:
-            #self.score += 2
+        else:
+            self.score += 2
 
-
-    def addGapPoints(self):
-        max_index = -1
-
-        for j in range(20):
-            for i in range(10):
-                if self.board[i][j] == BOARD_OCCUPIED:
-                    max_index = j
-                    break
-            if max_index != -1:
-                break
-
+    def dbgPoints(self):
+        score_to_add = 0
         for i in self.active_indices:
-            if max_index != -1 and i[1] >= max_index:
-                self.score += 1
+            if i[0] <= 4:
+                score_to_add = max(score_to_add, 5 - i[0])
             else:
-                self.score -= 9
+                score_to_add = max(score_to_add, 4 - i[0])
+        self.score += score_to_add
 
     def step(self, action):
         ret = True
@@ -577,7 +553,7 @@ class TetrisSimulation:
             self.moveRight()
         else:
             if not self.moveDown():
-                self.addGapPoints()
+                #self.dbgPoints()
                 for i in self.active_indices:
                     if i[1] >= 0:
                         self.board[i[0]][i[1]] = BOARD_OCCUPIED
@@ -586,7 +562,7 @@ class TetrisSimulation:
 
         if self.ticks_since_down >= 8:
             if not self.moveDown():
-                self.addGapPoints()
+                #self.dbgPoints()
                 for i in self.active_indices:
                     if i[1] >= 0:
                         self.board[i[0]][i[1]] = BOARD_OCCUPIED
@@ -595,101 +571,39 @@ class TetrisSimulation:
 
         return ret
 
-def clarifyDigit(im):
-    im = im.convert('L').getdata()
-    min_i = 0
-    diff = 0
-
-    for i in range(len(im)):
-        diff += abs(im[i] - masks[0][i])
-
-    for i in range(1, 10):
-        tmp_diff = 0
-        for j in range(len(im)):
-            tmp_diff += abs(im[j] - masks[i][j])
-
-        if diff > tmp_diff:
-            min_i = i
-            diff = tmp_diff
-
-    return min_i
-
-def getScore():
-    initial_x = 235
-    initial_y = 119
-
-    x_width = 8
-    y_width = 7
-
-    x_step = x_width + 1
-
-    score = 0
-
-    im = ImageGrab.grab((initial_x, initial_y, initial_x + 6 * x_width + 5, initial_y + y_width))
-    #im.show()
-
-    #Pull the score digits from the screen
-    for i in range(6):
-        bounding_box = (initial_x + i * x_step, initial_y, initial_x + i * x_step + x_width, initial_y + y_width)
-    #    score = score * 10 + clarifyDigit(ImageGrab.grab(bounding_box))
-        score = score * 10 + clarifyDigit(im.crop(bounding_box))
-
-    return score
-
-
-def getBoard():
-    initial_x = 126
-    initial_y = 103
-
-    x_step = 9
-    y_step = 8
-
-    board = np.ndarray((10, 20), dtype=np.float)
-
-    im = ImageGrab.grab((initial_x, initial_y, initial_x + 10 * x_step, initial_y + 20 * y_step))
-
-    initial_x = (x_step / 2)
-    initial_y = (y_step / 2)
-
-    for i in range(10):
-        for j in range(20):
-            p = im.getpixel((initial_x + x_step * i, initial_y + y_step * j))
-            if p[0] + p[1] + p[2] > 0:
-                board[i][j] = 1
-            else:
-                board[i][j] = 0
-
-    return board
-
-
-def getInput():
-    score = getScore()
-    board = getBoard()
-
-    return (score, board)
-
 
 class Memory:
     def __init__(self):
         self.clear()
 
     def clear(self):
-        self.memories = deque(maxlen=10000)
+        self.states = deque(maxlen=10000)
+        self.actions = deque(maxlen=10000)
+        self.rewards = deque(maxlen=10000)
 
     def addToMemory(self, new_observation, new_action, new_reward):
-        self.memories.append((new_observation, new_action, new_reward))
+        self.states.append(new_observation)
+        self.actions.append(new_action)
+        self.rewards.append(new_reward)
 
-    def sampleMemory(self, batch_size=64):
-        indices = np.random.choice(len(self.memories), batch_size, False)
+    def discountRewards(self, action_count, discount_rate = .99):
+        discounted_rewards = np.zeros_like(self.rewards)
+        R = 0
+        for t in reversed(range(len(self.rewards) - action_count, len(self.rewards))):
+            R = R * discount_rate + self.rewards[t]
+            self.rewards[t] = R
+
+    def sampleMemory(self, batch_size=256):
+        indices = np.random.choice(len(self.states), batch_size, False)
 
         observations = []
         actions = []
         rewards = []
 
         for i in indices:
-            observations.append(self.memories[i][0])
-            actions.append(self.memories[i][1])
-            rewards.append(self.memories[i][2])
+            observations.append(self.states[i])
+            actions.append(self.actions[i])
+            rewards.append(self.rewards[i])
 
         return np.array(observations), np.array(actions), np.array(rewards)
 
@@ -708,28 +622,9 @@ def createModel():
 
 
 def nextAction(model, observation):
-    #print(observation.shape)
     logits = model.predict(np.expand_dims(observation, (0, -1)))
 
     return logits.argmax()
-
-
-def normalize(x):
-    x -= np.mean(x)
-    x /= np.std(x)
-    return x.astype(np.float32)
-
-
-def discount_rewards(rewards, discount_rate = .99):
-    discounted_rewards = np.zeros_like(rewards)
-    R = 0
-    for t in reversed(range(0, len(rewards))):
-        R = R * discount_rate + rewards[t]
-        discounted_rewards[t] = R
-        if t - 1 >= 0 and rewards[t] == 0 and rewards[t - 1] != 0:
-            R = 0
-
-    return normalize(discounted_rewards)
 
 
 def compute_loss(logits, actions, rewards):
@@ -768,101 +663,39 @@ def syncModels(model, target, ratio):
             
             target.layers[layer_i].set_weights([weights, biases])
 
-
-def performAction(key):
-    pydirectinput.keyDown(key)
-    pydirectinput.keyDown('n')
-    pydirectinput.keyUp(key)
-    pydirectinput.keyUp('n')
-
-
 if __name__ == "__main__":
 
     board = TetrisSimulation()
 
-    flag = True
-
     model = createModel()
     target_model = createModel()
     memory = Memory()
-
-    supervised_step = False
-
-    if supervised_step == True:
-        while True:
-            flag == True
-            print("Supervised Learning Step:")
-            board.printBoard()
-            print(board.rotation_state)
-
-            observation = np.expand_dims(np.copy(board.board), axis=-1)
-            action = -1
-
-            c = msvcrt.getch()
-            os.system('cls')
-            old_score = board.score
-            if flag == False:
-                break
-            if c == b'a':
-                action = 2
-                flag = board.step("left")
-            elif c == b's':
-                action = 4
-                flag = board.step("down")
-            elif c == b'd':
-                action = 3
-                flag = board.step("right")
-            elif c == b'w':
-                action = 0
-                flag = board.step("rleft")
-            elif c == b'e':
-                action = 0
-                flag = board.step("rright")
-
-            reward = (board.score - old_score) * 1.0
-
-            if action != -1:
-                memory.addToMemory(observation, action, reward)
 
     learning_rate = 1e-4
     epsilon = 1.
     epsilon_decay_rate = .99999
     epsilon_min = .02
     optimizer = tf.keras.optimizers.Adam(learning_rate)
-    sync_frequency = 3
-    sync_rate = .4
+    sync_frequency = 10
+    sync_rate = 1.0
 
     sync_counter = 0
 
-    if os.path.isdir("model"):
-        model.load("model")
+    max_score = 0
 
-    if supervised_step == True:
-        observations, actions, rewards = memory.sampleMemory(len(memory.memories))
-        train_step(model, target_model, optimizer, observations, actions, discount_rewards(rewards))
-
-    #if hasattr(tqdm, '_instances'): tqdm._instances.clear()
     for i_episode in range(5000):
         observation = np.copy(board.board)
         print("Step: ", i_episode)
 
-        #count = 0
+        action_counter = 0
 
         while True:
             if random.random() < epsilon:
                 action = random.randint(0, 4)
             else:
                 action = nextAction(model, observation)
-            #count += 1
-            #print(action)
-
-            #if (count % 500) == 0:
-            #    board.printBoard()
-
-            #if count > 2000:
-            #    board.printBoard()
-            #    print(action)
-            #    print(board.ticks_since_down)
+            
+            action_counter += 1
 
             epsilon = max(epsilon * epsilon_decay_rate, epsilon_min)
 
@@ -871,16 +704,28 @@ if __name__ == "__main__":
             continue_flag = board.step(action_map[action])
             reward = (board.score - old_score) * 1.0
 
+            if not continue_flag:
+                reward -= 100
+
             new_observation = np.copy(board.board)
 
             memory.addToMemory(observation, action, reward)
 
             if not continue_flag:
-                observations, actions, rewards = memory.sampleMemory()
-                train_step(model, target_model, optimizer, observations, actions, discount_rewards(rewards))
+                memory.discountRewards(action_counter)
+                observations, actions, discounted_rewards = memory.sampleMemory()
+                train_step(model, target_model, optimizer, observations, actions, discounted_rewards)
 
                 #print("Step ", i_episode, " ended")
                 print("Score: ", board.score)
+
+                if board.score > max_score:
+                    max_score = board.score
+
+                print("Highscore: ", max_score)
+
+                board.printBoard()
+
                 board.clear()
                 break
 
